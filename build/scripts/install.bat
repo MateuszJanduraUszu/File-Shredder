a97fd93ad8bf213fd9f519ba7aadcce4b9d2ef07
MateuszJanduraUszu <Mati4321@interia.pl>
2024-01-01 12:07:12 +0100

    Fix registry key value in `install.bat` (#6)

--- build/scripts/install.bat
+++ build/scripts/install.bat
@@ -6,6 +6,6 @@
 @echo off
 set dir_path=%~dp0
 reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFile" /f /ve /d "Destroy file"
-reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFile\command" /f /ve /d "%dir_path%fshred.exe %%1 -d"
+reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFile\command" /f /ve /d "%dir_path%fshred.exe ""%%1"" -d"
 reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFileContents" /f /ve /d "Destroy file contents"
-reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFileContents\command" /f /ve /d "%dir_path%fshred.exe %%1"
+reg add "HKEY_CLASSES_ROOT\*\shell\DestroyFileContents\command" /f /ve /d "%dir_path%fshred.exe ""%%1""
