ca3a1e4403d719ada8773ed4b17398f7d8bcd718
MateuszJanduraUszu <Mati4321@interia.pl>
2024-01-01 13:35:54 +0100

    Add `<tinywin.hpp>` (#4)

---
+++ src/fshred/tinywin.hpp
@@ -0,0 +1,13 @@
+// tinywin.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _FSHRED_TINYWIN_HPP_
+#define _FSHRED_TINYWIN_HPP_
+
+#define WIN32_LEAN_AND_MEAN
+#define NOSERVICE
+#include <Windows.h>
+#endif // _FSHRED_TINYWIN_HPP_
